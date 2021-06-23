27688a88399a94a5f3a316b9bda6a9bca8de66b7
Marçal Astals Bota <marcal.asbo@gmail.com>
2021-06-23 16:02:35 +0200

    Initial commit

---
+++ app/src/main/java/com/sobren7km/tdaybyond/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sobren7km.tdaybyond
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
